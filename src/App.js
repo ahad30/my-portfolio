7e2901fe00c92c983ea71255332a747093b11712
ahad30 <mohiminulislamahad@gmail.com>
2023-04-28 02:24:11 +0600

    1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import './App.css';
 import All from './pages/All';
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Portfolio from "./pages/Portfolio";
-import Reviews from "./pages/Reviews";
-import Blog from "./pages/Blog";
-import Contact from "./pages/Contact";
 
 
 
