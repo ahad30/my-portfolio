a70146e9f2bbaa7225e609396a6120ba7fa1afca
ahad30 <mohiminulislamahad@gmail.com>
2023-04-28 01:43:55 +0600

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Portfolio from "./pages/Portfolio";
+import Reviews from "./pages/Reviews";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="/">Portfolio</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+                <Link to="/about" className="nav-link">
+                  About
+                </Link>
+                <Link to="/services" className="nav-link">
+                  Services
+                </Link>
+                <Link to="/portfolio" className="nav-link">
+                  Portfolio
+                </Link>
+                <Link to="/reviews" className="nav-link">
+                  Reviews
+                </Link>
+                <Link to="/blog" className="nav-link">
+                  Blog
+                </Link>
+                <Link to="/contact" className="nav-link">
+                  Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/services">
+            <Services />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route path="/reviews">
+            <Reviews />
+          </Route>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Jumbotron, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <Jumbotron>
+        <h1>Welcome to my Portfolio</h1>
+        <p>
+          Hi, I'm [Your Name] and I'm a [Your Profession]. Here you can find
+          more information about my skills, experience and previous projects.
+        </p>
+        <p>
+          <Link to="/portfolio">
+            <Button variant="primary">View my Portfolio</Button>
+          </Link>
+        </p>
+      </Jumbotron>
+    </div>
+  );
+}
+
+export default Home;
