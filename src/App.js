3f6afba01d61c512d2f8243a5fc8d8a726339c74
ahad30 <mohiminulislamahad@gmail.com>
2023-05-02 00:40:12 +0600

    1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import React from "react";
 import './App.css';
-import All from './pages/All';
-import Home from "./pages/Home";
+
+import Home from "./Components/home/Home";
+import About from "./Components/about/About";
+import Services from "./Components/services/Services";
+import Resume from "./Components/resume/Resume";
+import Portfolio from "./Components/portfolio/Portfolio";
+import Pricing from "./Components/pricing/Pricing";
+import Testimonials from  "./Components/testimonials/Testimonials";
+import Blog from "./Components/blog/Blog";
+import Contact from "./Components/contact/Contact";
 
 
 
 function App() {
   return (
-    <div className="App">
-      <All/>
+    <div className="App"> 
       <Home/>
+      <About/>
+      <Services/>
+      <Resume/>
+     <Portfolio/>
+      <Pricing/>
+      <Testimonials/>
+       <Blog/>
+      <Contact/>
     </div>
   );
 }
