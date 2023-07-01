f5a0f73241ef321af81b9fe50473ace7e72911ff
ahad30 <mohiminulislamahad@gmail.com>
2023-07-02 00:36:09 +0600

    more111

--- src/Components/testimonials/Testimonials.jsx
+++ src/Components/testimonials/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './testimonials.css';
 import Image1 from '../../assets/avatar-1.svg';
 import Image2 from '../../assets/avatar-3.svg';
@@ -7,7 +7,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-const data = [
+const testimonials = [
 
   {
     id: 1,
@@ -24,13 +24,25 @@ const data = [
     title: "Witkinson Henry",
     subtitle: "IT head at Wipro",
     comment:
-      "I enjoy working with the theme and learn so much and thats make the process fun and interesting, Good luck"
+      "Great working experience and outstanding design.Wish you a very good luck"
   },
 
 ]
 
 
 const Testimonials = () => {
+  const [currentTestimonial, setCurrentTestimonial] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTestimonial((prevTestimonial) => (prevTestimonial + 1) % testimonials.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [testimonials.length])
+
+
+
   return (
     <section className="testimonials container section">
       <h2 className="section__title">Client & Reviews</h2>
@@ -42,15 +54,15 @@ const Testimonials = () => {
      loop= {true}
      grabCursor={true}
       >
-        {data.map(({ id, image, title, subtitle, comment }) => {
+        {testimonials.map(({ id, image, title, subtitle, comment }) => {
           return (
             <SwiperSlide className="testimonial__item" key={id}>
               <div className="thumb">
-                <img src={image} alt="" />
+                <img src={testimonials[currentTestimonial].image} alt="" />
               </div>
-              <h3 className='testimonials__title'>{title}</h3>
-              <span className='subtitle'>{subtitle}</span>
-              <div className="comment">{comment}</div>
+              <h3 className='testimonials__title'>{testimonials[currentTestimonial].title}</h3>
+              <span className='subtitle'>{testimonials[currentTestimonial].subtitle}</span>
+              <div className="comment">{testimonials[currentTestimonial].comment}</div>
             </SwiperSlide>
           )
 })}
